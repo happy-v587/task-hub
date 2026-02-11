a32cffd3bde99f0ff4fc33f5db36cc81e2677597
Happy <code4happy@gmail.com>
2026-02-11 13:34:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: "./",
+  base: "./task-hub/",
   plugins: [
     vue(),
     tailwindcss(),
